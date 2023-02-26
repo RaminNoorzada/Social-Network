889fa30c909fe1e5ca12b92ee65a11926fa9c7a8
Your Name <you@example.com>
2023-02-26 00:21:03 -0800

    draft files added.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { default: test } = require('node:test');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//pp.use(routes);
+app.get('/api/test', (req, res) => {
+    res.send('test');
+    return;
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
